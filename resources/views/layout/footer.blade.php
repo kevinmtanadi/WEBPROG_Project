d6e1c434d5b506df01fc406fd205c208056ff401
kevinmt1 <kevinmt2342@gmail.com>
2022-12-28 13:30:37 +0700

    Added login page

---
+++ resources/views/layout/footer.blade.php
@@ -0,0 +1,45 @@
+
+<section class="mt-auto">
+    <footer class="p-2 text-white text-center position-relative bg-1">
+        <div class="mt-2">
+            <a href="/"><h1>MovieList</h1></a>
+        </div>
+
+        <div class="mt-1">
+            MoviesList is a Website that contains list of movie
+        </div>
+        <div class="social-media my-2">
+            <a class="social-media-item mx-2 text-white" href="">
+                <i class="fa fa-twitter" style="font-size:24px"></i>
+            </a>
+            <a class="social-media-item mx-2 text-white" href="">
+                <i class="fa fa-instagram" style="font-size:24px"></i>
+            </a>
+            <a class="social-media-item mx-2 text-white" href="">
+                <i class="fa fa-facebook" style="font-size:24px"></i>
+            </a>
+            <a class="social-media-item mx-2 text-white" href="">
+                <i class="fa fa-reddit" style="font-size:24px"></i>
+            </a>
+            <a class="social-media-item mx-2 text-white" href="">
+                <i class="fa fa-youtube" style="font-size:24px"></i>
+            </a>
+        </div>
+        <div class="info my-2">
+            <ul class="d-flex justify-content-center">
+                <li class="list-unstyled mx-2"><a href="" class="text-white">Privacy Policy</a></li>|
+                <li class="list-unstyled mx-2"><a href="" class="text-white">Terms of Service</a></li>|
+                <li class="list-unstyled mx-2"><a href="" class="text-white">Contact Us</a></li>|
+                <li class="list-unstyled mx-2"><a href="" class="text-white">About Us</a></li>|
+            </ul>
+        </div>
+        <div class="my-2 mb-4">
+            Copyright &copy; 2021 MovieList All Rights Reserved
+        </div>
+
+    </footer>
+</section>
+
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js" integrity="sha384-cuYeSxntonz0PPNlHhBs68uyIAVpIIOZZ5JqeqvYYIcEL727kskC66kF92t6Xl2V" crossorigin="anonymous"></script>
+
---
+++ resources/views/layout/navbar.blade.php
@@ -0,0 +1,24 @@
+<section>
+        <nav class="navbar navbar-expand fixed-top justify-content-around py-3 bg-1">
+            <div class="text-white">
+                MovieList
+            </div>
+            <ul class="navbar-nav">
+                <li class="nav-item mx-2 my-auto">
+                    <a href="/" class="text-white">Home</a>
+                </li>
+                <li class="nav-item mx-2 my-auto">
+                    <a href="/movies" class="text-white">Movies</a>
+                </li>
+                <li class="nav-item mx-2 my-auto">
+                    <a href="/contact" class="text-white">Actors</a>
+                </li>
+                <li class="nav-item mx-2 my-auto">
+                    <a href="/register" class="btn btn-primary">Register</a>
+                </li>
+                <li class="nav-item mx-2 my-auto">
+                    <a href="/login" class="btn btn-outline-primary">Login</a>
+                </li>
+            </ul>
+        </nav>
+</section>
